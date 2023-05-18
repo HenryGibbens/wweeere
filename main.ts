3ad96695b361e14bf184d9433020eee80b13a482
HenryGibbens <79278548+HenryGibbens@users.noreply.github.com>
2023-05-18 10:43:00 +0100

    we added more things on for our mini robot

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("backwards")
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.West)
+    radio.sendString("left")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("forward")
+    basic.showArrow(ArrowNames.North)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("stop")
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendString("right")
+    basic.showArrow(ArrowNames.West)
+})
+radio.setGroup(123)
 basic.forever(function () {
 	
 })
